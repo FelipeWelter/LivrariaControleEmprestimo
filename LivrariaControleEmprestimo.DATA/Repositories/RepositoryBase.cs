cd80aa7b54b012bca4dfc343fdd0e1b5fe9f0018
WELTER <WELTER@DESKTOP-5OUGR9J>
2023-05-30 19:21:03 -0300

    Adicionar arquivos de projeto.

---
+++ LivrariaControleEmprestimo.DATA/Repositories/RepositoryBase.cs
@@ -0,0 +1,78 @@
+﻿using LivrariaControleEmprestimo.DATA.Interfaces;
+using LivrariaControleEmprestimo.DATA.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LivrariaControleEmprestimo.DATA.Repositories
+{
+    public class RepositoryBase<T> : IRepositoryModel<T>, IDisposable where T : class
+    {
+        protected ControleEmprestimoLivroContext _Contexto;
+        public bool _SaveChanges = true;
+
+        public RepositoryBase(bool saveChanges = true) 
+        {
+            _SaveChanges = saveChanges;
+            _Contexto = new ControleEmprestimoLivroContext();
+        }
+
+        public T Alterar(T objeto)
+        {
+            _Contexto.Entry(objeto).State = EntityState.Modified;
+            if (_SaveChanges)
+            {
+                _Contexto.SaveChanges();
+            }
+            return objeto;
+        }
+
+        public void Dispose()
+        {
+           _Contexto.Dispose();
+        }
+
+        public void Excluir(T objeto)
+        {
+            _Contexto.Set<T>().Remove(objeto);
+            if (_SaveChanges)
+            {
+                _Contexto.SaveChanges();
+            }
+        }
+
+        public void Excluir(params object[] variavel)
+        {
+            var objeto = SelecionarPK(variavel);
+            Excluir(objeto);
+        }
+
+        public T Incluir(T objeto)
+        {
+            _Contexto.Set<T>().Add(objeto);
+            if (_SaveChanges)
+            {
+                _Contexto.SaveChanges();
+            }
+            return objeto;
+        }
+
+        public void SaveChages()
+        {
+            _Contexto.SaveChanges();
+        }
+
+        public T SelecionarPK(params object[] variavel)
+        {
+            return _Contexto.Set<T>().Find(variavel);
+        }
+
+        public List<T> SelecionarTodos()
+        {
+            return _Contexto.Set<T>().ToList();
+        }
+    }
+}
---
+++ LivrariaControleEmprestimo.WEB/Controllers/ClienteController.cs
@@ -0,0 +1,62 @@
+﻿using LivrariaControleEmprestimo.DATA.Models;
+using LivrariaControleEmprestimo.DATA.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace LivrariaControleEmprestimo.WEB.Controllers
+{
+    
+    public class ClienteController : Controller
+    {
+        private ClienteService oClienteService = new ClienteService();
+        public IActionResult Index()
+        {
+            List<Cliente> oListClient = oClienteService.oRepositoryCliente.SelecionarTodos();
+            return View(oListClient);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Cliente model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            
+            oClienteService.oRepositoryCliente.Incluir(model);
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Details(int id)
+        {
+            Cliente oCliente = oClienteService.oRepositoryCliente.SelecionarPK(id);
+            return View(oCliente);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            Cliente oCliente = oClienteService.oRepositoryCliente.SelecionarPK(id);
+            return View(oCliente);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Cliente model)
+        {
+            Cliente oCliente = oClienteService.oRepositoryCliente.Alterar(model);
+            int id = oCliente.Id;
+            return RedirectToAction("Details", new { id });
+        }
+
+        public IActionResult Delete(int id)
+        {
+            oClienteService.oRepositoryCliente.Excluir(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
